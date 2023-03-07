f7a7ec527609b565df7abf4ce67d4c5c0f4fe338
Cliff Chill <maxim.magaz@gmail.com>
2023-03-07 17:08:11 +0100

    Added required pages

---
+++ BookshopWPF/Bookshop/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bookshop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            Frame1.Content = new WelcomePage();
+        }
+
+        private void Checkout_Click(object sender, RoutedEventArgs e)
+        {
+            Frame1.Content = new CheckoutPage();
+        }
+
+        private void Report_Click(object sender, RoutedEventArgs e)
+        {
+            Frame1.Content = new ReportPage();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame1.Content = new AddBookPage();
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame1.Content = new EditPage();
+        }
+
+        private void ReceiptButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame1.Content = new ReceiptPage();
+        }
+
+        private void MissingButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame1.Content = new MissingPage();
+        }
+    }
+}
